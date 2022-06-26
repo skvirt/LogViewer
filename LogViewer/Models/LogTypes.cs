9073fae7a0452469855cda21a691f52d3fbf27e7
Blann7 <blann.n@yandex.ru>
2022-06-26 09:02:54 +0300

    fix

--- LogViewer/Models/LogTypes.cs
+++ LogViewer/Models/LogTypes.cs
@@ -7,16 +7,14 @@ namespace LogViewer.Models
         public enum LogType
         {
             [Display(Name = "Не выбрано")] NONE, [Display(Name = "Покупка роли")] BUY_ROLE, [Display(Name = "Роль истекла")] EXPIRED_ROLE,
-            [Display(Name = "Пополнение денег")] GET_MONEY, [Display(Name = "Трата денег")] SPENT_MONEY, 
-            [Display(Name = "Вход")] LOGIN, [Display(Name = "Выход")] LOGOUT, 
+            [Display(Name = "Пополнение денег")] GET_MONEY, [Display(Name = "Вход")] LOGIN, [Display(Name = "Выход")] LOGOUT,
             [Display(Name = "Реферал приглашён")] REFERRAL_INVITE, [Display(Name = "Создана реф. ссылка")] REFERRAL_GENERATE_NEW_LINK,
-            [Display(Name = "Блокировка")] BAN, [Display(Name = "Разблокировка")] UNBAN, 
-            [Display(Name = "about us ред.")] EDIT_ABOUT_US, [Display(Name = "Изменение пользователя")] EDIT_USER, 
-            [Display(Name = "Изменение руководства")] EDIT_HEADS, [Display(Name = "Изменение рефералки")] EDIT_REFERRAL, 
-            [Display(Name = "Удаление рефералки")] DELETE_REFERRAL, [Display(Name = "Добавление статьи")] ADD_ARTICLE,
-            [Display(Name = "Изменение статьи")] CHANGE_ARTICLE, [Display(Name = "Удаление статьи")] DELETE_ARTICLE,
-            [Display(Name = "Изменение IP")] CHANGE_IP, [Display(Name = "Регистрация")] REGISTER_USER,
-            [Display(Name = "Снятие руководства")] REMOVE_HEADS
+            [Display(Name = "Блокировка")] BAN, [Display(Name = "Разблокировка")] UNBAN, [Display(Name = "Регистрация")] REGISTER_USER,
+            [Display(Name = "about us ред.")] EDIT_ABOUT_US, [Display(Name = "Изменение пользователя")] EDIT_USER,
+            [Display(Name = "Изменение руководства")] EDIT_HEADS, [Display(Name = "Снятие руководства")] REMOVE_HEADS,
+            [Display(Name = "Изменение рефералки")] EDIT_REFERRAL, [Display(Name = "Удаление рефералки")] DELETE_REFERRAL,
+            [Display(Name = "Добавление статьи")] ADD_ARTICLE, [Display(Name = "Изменение статьи")] CHANGE_ARTICLE,
+            [Display(Name = "Удаление статьи")] DELETE_ARTICLE
         }
         public static readonly Dictionary<LogType, string> logTypesKeyEnum = new()
         {
@@ -24,9 +22,9 @@ namespace LogViewer.Models
             [LogType.BUY_ROLE] = "Покупка роли",
             [LogType.EXPIRED_ROLE] = "Роль истекла",
             [LogType.GET_MONEY] = "Пополнение денег",
-            [LogType.SPENT_MONEY] = "Трата денег",
             [LogType.LOGIN] = "Вход",
             [LogType.LOGOUT] = "Выход",
+            [LogType.REGISTER_USER] = "Регистрация",
             [LogType.REFERRAL_INVITE] = "Реферал приглашён",
             [LogType.REFERRAL_GENERATE_NEW_LINK] = "Создана реф. ссылка",
             [LogType.BAN] = "Блокировка",
@@ -39,9 +37,7 @@ namespace LogViewer.Models
             [LogType.DELETE_REFERRAL] = "Удаление рефералки",
             [LogType.ADD_ARTICLE] = "Добавление статьи",
             [LogType.CHANGE_ARTICLE] = "Изменение статьи",
-            [LogType.DELETE_ARTICLE] = "Удаление статьи",
-            [LogType.CHANGE_IP] = "Изменение IP",
-            [LogType.REGISTER_USER] = "Регистрация"
+            [LogType.DELETE_ARTICLE] = "Удаление статьи"
         };
     }
 }
